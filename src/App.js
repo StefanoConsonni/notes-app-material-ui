d373a7b2409b5372aa954cde1640adca3a713289
Stefano Consonni <stefanoconsonni@ymail.com>
2022-10-21 12:34:55 +0200

    add custom theme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { purple } from "@mui/material/colors";
+
+// components
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#fefefe",
+		},
+		secondary: {
+			main: purple[500],
+		},
+	},
+	typography: {
+		fontFamily: "Quicksand",
+		fontWeightLight: 400,
+		fontWeightRegular: 500,
+		fontWeightMedium: 600,
+		fontWeighBold: 700,
+	},
+});
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<Notes />
+					</Route>
+					<Route path="/create">
+						<Create />
+					</Route>
+				</Switch>
+			</Router>
+		</ThemeProvider>
+	);
 }
 
 export default App;
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,12 +1,35 @@
-import { Typography } from "@mui/material";
 import React from "react";
+import { Typography, Button, Container } from "@mui/material";
+import { makeStyles } from "tss-react/mui";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+// import SendIcon from "@mui/icons-material/Send";
+
+const useStyles = makeStyles()(() => {
+	return {
+		btn: {
+			backgroundColor: "secondary",
+		},
+	};
+});
 
 export default function Create() {
+	const { classes } = useStyles();
+
 	return (
-		<div>
-			<Typography variant="h6" component="h2" color="textSecondary" align="center" gutterBottom>
+		<Container>
+			<Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
 				Create a New Note
 			</Typography>
-		</div>
+
+			<Button
+				type="submit"
+				className={classes.btn}
+				color="secondary"
+				variant="contained"
+				endIcon={<KeyboardArrowRightIcon />}
+				onClick={() => console.log("you clicked this button!")}>
+				Submit
+			</Button>
+		</Container>
 	);
 }
